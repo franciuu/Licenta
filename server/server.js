4c6e6e59c81c901bb29f5583f12dee8c5abf2917
Balint Franceska <balintfranceska@gmail.com>
2024-12-11 15:19:29 +0200

    inca cv

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 import express from "express";
 import session from "express-session";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
-const app = express()
+const app = express();
+
+app.use(session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: 'auto'
+    }
+}));
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+app.use(express.json());
 
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
