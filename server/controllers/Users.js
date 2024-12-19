4f461104c3cb1703f4b100aae0b448df615fd997
Balint Franceska <balintfranceska@gmail.com>
2024-12-19 12:47:35 +0200

    bv franci s-a rezolvat cu conectarea la baza de date

---
+++ server/controllers/Users.js
@@ -0,0 +1,46 @@
+import User from "../models/User.js";
+import argon2 from "argon2";
+
+export const getUsers = async (req, res) => {
+  try {
+    const response = await User.findAll({
+      attributes: ["uuid", "name", "email", "role"],
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+export const getUserById = async (req, res) => {
+  try {
+    const response = await User.findOne({
+      attributes: ["uuid", "name", "email", "role"],
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+export const createUser = async (req, res) => {
+  const { name, email, password, confPassword, role } = req.body;
+  if (password !== confPassword) {
+    return res.status(400).json({ msg: "Password don't match" });
+  }
+  const hashPassword = await argon2.hash(password);
+  try {
+    await User.create({
+      name: name,
+      email: email,
+      password: hashPassword,
+      role: role,
+    });
+    res.status(201).json({ msg: "Register sucessful" });
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+export const updateUser = (req, res) => {};
+export const deleteUser = (req, res) => {};
--- server/server.js
+++ server/server.js
@@ -2,29 +2,50 @@ import express from "express";
 import session from "express-session";
 import dotenv from "dotenv";
 import cors from "cors";
+import UserRoute from "./routes/UserRoute.js";
+import db from "./config/database.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESS_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: 'auto'
-    }
-}));
-app.use(cors({
+      secure: "auto",
+    },
+  })
+);
+app.use(
+  cors({
     credentials: true,
-    origin: 'http://localhost:3000'
-}));
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
+(async () => {
+  try {
+    await db.authenticate();
+    console.log("Conexiune reușită la baza de date!");
+
+    // Creează automat tabelele definite în modele
+    await db.sync();
+    console.log("Toate tabelele au fost sincronizate.");
+  } catch (error) {
+    console.error("Eroare la conectarea bazei de date:", error.message);
+  }
+})();
+
 app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
-})
+  res.json({ users: ["userOne", "userTwo", "userThree"] });
+});
+
+app.use(UserRoute);
 
 app.listen(process.env.PORT, () => {
-    console.log("server started on port 5000")
+  console.log("server started on port 5000");
 });
