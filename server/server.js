e562498df211b4be64399f2a4a2eb8943312c2b2
Balint Franceska <balintfranceska@gmail.com>
2024-12-22 15:23:16 +0200

    m-am mutat pe client la autentificare

--- server/server.js
+++ server/server.js
@@ -2,18 +2,26 @@ import express from "express";
 import session from "express-session";
 import dotenv from "dotenv";
 import cors from "cors";
-import UserRoute from "./routes/UserRoute.js";
+import SequelizeStore from "connect-session-sequelize";
 import db from "./config/database.js";
+import UserRoute from "./routes/UserRoute.js";
+import AuthRoute from "./routes/AuthRoute.js";
 
 dotenv.config();
 
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+const store = new sessionStore({
+  db: db,
+});
 
 app.use(
   session({
     secret: process.env.SESS_SECRET,
     resave: false,
     saveUninitialized: true,
+    store: store,
     cookie: {
       secure: "auto",
     },
@@ -45,6 +53,7 @@ app.get("/api", (req, res) => {
 });
 
 app.use(UserRoute);
+app.use(AuthRoute);
 
 app.listen(process.env.PORT, () => {
   console.log("server started on port 5000");
